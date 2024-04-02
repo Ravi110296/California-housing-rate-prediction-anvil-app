bf8b94cd26ffaf97602e751f017d351a503c6514
ravissharma112@gmail.com <ravissharma112@gmail.com>
2024-04-02 10:13:23 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,10 +12,10 @@ class Form1(Form1Template):
   def Predict_click(self, **event_args):
     """This method is called when the button is clicked"""
     prediction = anvil.server.call('House_Prediction',
-                                  self.MedInc,self.HouseAge,
-                                  self.AveRooms,self.AveBedrms,
-                                  self.Population,self.AveOccup,
-                                  self.Latitude,self.Longitude)
+                                  self.MedInc.text,self.HouseAge.text,
+                                  self.AveRooms.text,self.AveBedrms.text,
+                                  self.Population.text,self.AveOccup.text,
+                                  self.Latitude.text,self.Longitude.text)
     if prediction:
       self.Prediction.visible = True
       self.Prediction.text = 'The predicted house price is {}'.format(round(prediction,2))
