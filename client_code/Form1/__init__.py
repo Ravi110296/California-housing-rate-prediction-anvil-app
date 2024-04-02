71c61dfb7228794fd26d07da320583256e58008e
ravissharma112@gmail.com <ravissharma112@gmail.com>
2024-04-02 08:22:58 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def Predict_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    prediction = anvil.server.call('House_Prediction',
+                                  self.MedInc,self.HouseAge,
+                                  self.AveRooms,self.AveBedrms,
+                                  self.Population,self.AveOccup,
+                                  self.Latitude,self.Longitude)
+    if prediction:
+      self.Prediction.visible = True
+      self.Prediction.text = 'The predicted house price is {}'.format(round(prediction,2))
